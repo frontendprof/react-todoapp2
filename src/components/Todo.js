ae171fc5e912643a0b2defdb04164b516ced9ac9
frontendprof <abdumaliksharipov@gmail.com>
2020-10-10 23:04:55 +0500

    Adding a class on toggle

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,10 +8,21 @@ const Todo=({text,todos,todo,setTodos})=> {
         setTodos(todos.filter(el=>el.id!==todo.id));
     }
 
+    const completeItemHandler=()=>{
+        setTodos(todos.map(item=>{
+            if(item.id===todo.id){
+                return {...item,completed:!item.completed}
+            }
+            return item;
+        }))
+    }
+
     return (
         <div className="todo">
-            <li className="todo-item">{text}</li>
-            <button className="complete-btn"><i className="fas fa-check"></i></button>
+            <li className={`todo-item ${todo.completed?"completed":""}`}>{text}</li>
+            <button
+            onClick={completeItemHandler}
+             className="complete-btn"><i className="fas fa-check"></i></button>
             <button 
             onClick={deleteItemHandler}
             className="trash-btn"><i className="fas fa-trash"></i></button>
